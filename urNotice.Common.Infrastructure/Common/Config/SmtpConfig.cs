1d211acaffa15f4684f1aa78a5ce9218bc6374ee
Sumit Chourasia <sumitchourasia91@gmail.com>
2016-07-31 08:34:53 +0100

    Update SmtpConfig.cs

--- urNotice.Common.Infrastructure/Common/Config/SmtpConfig.cs
+++ urNotice.Common.Infrastructure/Common/Config/SmtpConfig.cs
@@ -9,7 +9,7 @@ namespace urNotice.Common.Infrastructure.Common.Config
     public class SmtpConfig
     {
         //public const string SmtpEmail = "OrbitPage";
-        //public const string SmtpPassword = "E31wApla1AJ5_Khl9v3Ocw";
+        //public const string SmtpPassword = "E31wApla1AJ5sumit_Khl9v3Ocw";
         //public const string SmtpPort = "587";
         //public const string SmtpHost = "smtp.mandrillapp.com";
         //public const string SmtpEnableSsl = "true";
@@ -17,7 +17,7 @@ namespace urNotice.Common.Infrastructure.Common.Config
         //public const string SmtpEmailFromDoNotReply = "donotreply@orbitpage.com";
 
         public const string SmtpEmail = "orbitpage@gmail.com";
-        public const string SmtpPassword = "pass-dream";
+        public const string SmtpPassword = "password";
         public const string SmtpPort = "587";
         public const string SmtpHost = "smtp.gmail.com";
         public const string SmtpEnableSsl = "true";
@@ -25,7 +25,7 @@ namespace urNotice.Common.Infrastructure.Common.Config
         public const string SmtpEmailFromDoNotReply = "orbitpage@gmail.com";
 
         public const string GmailSmtpEmail = "zestork@gmail.com";
-        public const string GmailSmtpPassword = "welcome12#";
+        public const string GmailSmtpPassword = "password";
         public const string GmailSmtpPort = "587";
         public const string GmailSmtpHost = "smtp.gmail.com";
         public const string GmailSmtpEnableSsl = "true";
