0df9865fe7c3efffbaf7db46ca463aab800d9fa2
Sumit Chourasia <sumitchourasia91@gmail.com>
2016-07-31 08:33:29 +0100

    Update ServerConfig.cs

--- urNotice.Common.Infrastructure/Common/Config/ServerConfig.cs
+++ urNotice.Common.Infrastructure/Common/Config/ServerConfig.cs
@@ -5,20 +5,20 @@
 
         public static string ContactUsReceivingEmailIds = "sumitchourasia91@gmail.com,sum_kumar12@yahoo.co.in";
 
-        public static string linkedinAppID = "750h4mf7rnwgtp";
-        public static string linkedinAppSecret = "J4QYHXXNGIYfDEjy";
+        public static string linkedinAppID = "750h4mfsumit7rnwgtp";
+        public static string linkedinAppSecret = "J4QYHXXsumitNGIYfDEjy";
 
-        public static string FacebookAppID = "952821118143036";
-        public static string FacebookAppSecret = "f070ab8f504d72f8b480c0d547a3a615";
+        public static string FacebookAppID = "95282111sumit8143036";
+        public static string FacebookAppSecret = "f070ab8f504d72f8b4sumit80c0d547a3a615";
 
-        public static string FacebookAppIDCautom = "1047990825261876";
-        public static string FacebookAppSecretCautom = "3479d5667f8489258c4cbf9fe68d1740";
+        public static string FacebookAppIDCautom = "10479908sumit25261876";
+        public static string FacebookAppSecretCautom = "3479d5667f84892sumit58c4cbf9fe68d1740";
 
-        public static string googleAppID = "196278631593-cbcnujqm3rsn1s0ugmno87q7fnbomani.apps.googleusercontent.com";
-        public static string googleAppSecret = "iJul5EtZD0DWurjZurhn_k1I";
+        public static string googleAppID = "196278631593-cbcnujqm3rsn1ssumit0ugmno87q7fnbomani.apps.googleusercontent.com";
+        public static string googleAppSecret = "iJul5EtZD0DsumitWurjZurhn_k1I";
 
-        public static string googleAppIDCautom = "366145181713-ro4kmuumfnkt1rub8f1gr4pacq52ijhq.apps.googleusercontent.com";
-        public static string googleAppSecretCautom = "WYCWZsI1JCZuPPexJlJqs7Ha";
+        public static string googleAppIDCautom = "366145181713-ro4kmuumfnkt1rub8fsumit1gr4pacq52ijhq.apps.googleusercontent.com";
+        public static string googleAppSecretCautom = "WYCWZsI1JCZuPPsumitexJlJqs7Ha";
 
     }
 }
